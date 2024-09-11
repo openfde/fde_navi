986b295b9912aaf166686aab8e2e6c49c265cee1
gongyong <185457686@qq.com>
2024-09-11 15:22:09 +0800

    fix the move arbitary

--- main.cpp
+++ main.cpp
@@ -1,80 +1,86 @@
-#include <QApplication>
-#include <QPushButton>
-#include <QScreen>
 #include <QWidget>
+#include <QPushButton>
 #include <QMouseEvent>
-#include <QIcon>
+#include <QApplication>
+#include <QScreen>
+#include <QDebug>
 
-class FloatingButton : public QPushButton {
+
+class DraggableButton : public QWidget {
 public:
-    FloatingButton(const QString &text, const QString &iconPath, QWidget *parent = nullptr)
-        : QPushButton(text, parent), otherButton(nullptr) {
-        setFixedSize(100, 100); // 设置按钮大小
-        setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint); // 无边框，始终在最上层
-        setAttribute(Qt::WA_TranslucentBackground); // 背景透明
-        setIcon(QIcon(iconPath)); // 设置图标
-        setIconSize(QSize(60, 60)); // 设置图标大小
+    DraggableButton(const QString &iconPath, QWidget *parent = nullptr) : QWidget(parent) {
+        button = new QPushButton("", this);
+        //设置为无框透明，且总在最上面
+        // button->setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+        setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+        button->setIcon(QIcon(iconPath));
+        button->setIconSize(QSize(80, 80));
+        button->setFixedSize(100, 100);
+        button->move(0, 0);
+        button->setMouseTracking(true);
+        setMouseTracking(true);
     }
 
-    void setOtherButton(FloatingButton *button) {
-        otherButton = button;
+    void setOtherButton(DraggableButton *otherButton) {
+        this->otherButton = otherButton;
     }
 
 protected:
-    void mousePressEvent(QMouseEvent *event) override {
-        if (event->button() == Qt::LeftButton) {
-            dragging = true;
-            dragStartPosition = event->globalPos() - pos();
-        }
+
+void mousePressEvent(QMouseEvent *event) override {
+    qDebug()<<"press "<<pos();
+    if (event->button() == Qt::LeftButton) {
+        dragging = true;
+        dragStartPosition = event->globalPos() - frameGeometry().topLeft();
     }
-
-    void mouseMoveEvent(QMouseEvent *event) override {
-        if (dragging && (event->buttons() & Qt::LeftButton)) {
-            QPoint newPos = event->globalPos() - dragStartPosition;
-
-            // 限制左右移动，只允许上下移动
-            move(x(), newPos.y());
-
-            // 同步另一个按钮的位置
-            if (otherButton) {
-                otherButton->move(otherButton->x(), newPos.y());
-            }
-        }
-    }
-
-    void mouseReleaseEvent(QMouseEvent *event) override {
-        if (event->button() == Qt::LeftButton) {
-            dragging = false;
-        }
-    }
-
-private:
-    QPoint dragStartPosition;
-    bool dragging = false;
-    FloatingButton *otherButton;
-};
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-
-    // 获取屏幕的几何信息
-    QRect screenGeometry = QGuiApplication::primaryScreen()->geometry();
-
-    // 创建左侧悬浮按钮，添加向左箭头图标
-    FloatingButton *leftButton = new FloatingButton("", ":/images/left.png");
-    leftButton->move(10, (screenGeometry.height() - leftButton->height()) / 2); // 左侧位置
-    leftButton->show(); // 显示按钮
-
-    // 创建右侧悬浮按钮，添加向右箭头图标
-    FloatingButton *rightButton = new FloatingButton("", ":/images/right.png");
-    rightButton->move(screenGeometry.width() - rightButton->width() - 10,
-                       (screenGeometry.height() - rightButton->height()) / 2); // 右侧位置
-    rightButton->show(); // 显示按钮
-
-    // 设置两个按钮互相知道对方
-    leftButton->setOtherButton(rightButton);
-    rightButton->setOtherButton(leftButton);
-
-    return app.exec();
 }
 
+void mouseMoveEvent(QMouseEvent *event) override {
+    if (dragging && (event->buttons() & Qt::LeftButton)) {
+        QPoint newPos = event->globalPos() - dragStartPosition;
+
+        // 限制左右移动，只允许上下移动
+        move(pos().x(), newPos.y());
+
+        // 同步另一个按钮的位置
+        if (otherButton) {
+            otherButton->move(otherButton->pos().x(), newPos.y());
+        }
+    }
+}
+
+// void mouseReleaseEvent(QMouseEvent *event) override {
+//     if (event->button() == Qt::LeftButton) {
+//         dragging = false;
+//     }
+// }
+
+
+
+private:
+    QPushButton *button;
+    QPoint dragStartPosition;
+    bool dragging = true;
+    DraggableButton *otherButton = nullptr;
+};
+
+//添加main方法
+int main(int argc , char * argv[]){
+    QApplication app(argc, argv);
+    DraggableButton *lbtn = new DraggableButton(":/images/left.png");
+    DraggableButton *draggableButton = new DraggableButton(":/images/right.png");
+    //设置draggableButton的大小为100 * 100
+    draggableButton->setFixedSize(100, 100);
+    lbtn->setFixedSize(100, 100);
+    QRect screenRect = QGuiApplication::primaryScreen()->geometry();
+    //draggableButton 移动到屏幕的右侧中间
+    draggableButton->move(screenRect.width() - draggableButton->width(), screenRect.height() / 2 - draggableButton->height() / 2);
+    //lbtn 移动到屏幕的左侧中间
+    lbtn->move(0, screenRect.height() / 2 - lbtn->height() / 2);
+    //设置draggableButton的另一个按钮为lbtn
+    draggableButton->setOtherButton(lbtn);
+    lbtn->show();
+    draggableButton->show();
+    lbtn->setOtherButton(draggableButton);
+    return app.exec();
+}
