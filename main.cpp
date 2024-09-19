b269facb5d1780aa64793748efbf25912a56d286
185457686 <185457686@qq.com>
2024-09-19 15:41:08 +0800

    formated

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <QWidget>
-#include <QPainter>
-#include <QRegion>
 #include <QPushButton>
 #include <QTimer>
-#include <QPropertyAnimation>
 #include <QMouseEvent>
 #include <QApplication>
-#include <QVBoxLayout>
 #include <QScreen>
 #include <QDebug>
 #include<QDateTime>
@@ -26,16 +22,16 @@ public:
     DraggableButton(ArrayDirection direction, const QString &iconPath, QRect rect, QWidget *parent = nullptr) : QWidget(parent) {
         //设置为无框tool 模式，且总在最上面
         setWindowFlags(Qt::FramelessWindowHint |Qt::Tool|  Qt::WindowStaysOnTopHint);
-	setAttribute(Qt::WA_StyledBackground);
+	    setAttribute(Qt::WA_StyledBackground);
         setAttribute(Qt::WA_TranslucentBackground);
 
         button = new QPushButton("", this);
-	button->setStyleSheet("QPushButton{border-radius: 15px; }");
+	    button->setStyleSheet("QPushButton{border-radius: 15px; }");
         button->setIcon(QIcon(iconPath));
         button->setIconSize(QSize(80, 80));
         button->setFixedSize(100, 100);
         reservedIconPath = iconPath;
-	reservedRect = rect;
+	    reservedRect = rect;
         //connect button的按下事件和一个槽函数
         connect(button, &QPushButton::pressed, this, &DraggableButton::onButtonPressed);
         //connect button的释放事件和一个槽函数
@@ -45,57 +41,43 @@ public:
         setMouseTracking(true);
         //标记自己的方向
         arrayDirection = direction;
-	 QTimer::singleShot(3000, this, [this](){
+	    QTimer::singleShot(3000, this, [this](){
             moveByHoverLeaved();
         });
     }
+
     void setOtherButton(DraggableButton *otherButton) {
         this->otherButton = otherButton;
     }
 
 
-    /*void paintEvent(QPaintEvent *event) override {
-        QPainter painter(this);
-        painter.setRenderHint(QPainter::Antialiasing);
-
-        painter.setBrush(QColor(192,192,192)); // 灰色填充色
-        painter.drawRoundedRect(rect(), 15,15); // 绘制带圆角的矩形
-    }
-
-    void resizeEvent(QResizeEvent *event) override {
-        QRegion region = QRegion(0, 0, width(), height(), QRegion::Rectangle);
-        setMask(region);
-    }
-    */
-
-
     void moveByHoverEntered() {
         if ( (arrayDirection == RIGHT && movedRight) || (arrayDirection == LEFT) ) {
-                button->setFixedSize(100,100);
-                setFixedSize(100,100);
-                button->setIcon(QIcon(reservedIconPath));
-                button->setIconSize(QSize(80,80));
-		button->setStyleSheet("QPushButton{border-radius: 15px; }");
-	}
+            button->setFixedSize(100,100);
+            setFixedSize(100,100);
+            button->setIcon(QIcon(reservedIconPath));
+            button->setIconSize(QSize(80,80));
+		    button->setStyleSheet("QPushButton{border-radius: 15px; }");
+	    }
         if ((arrayDirection == RIGHT && movedRight) ){
-                move(reservedRect.width()-100, pos().y());
-                movedRight = false;
+            move(reservedRect.width()-100, pos().y());
+            movedRight = false;
         }
     }
 
     void moveByHoverLeaved() {
         if ( (arrayDirection == RIGHT && !movedRight) || (arrayDirection == LEFT) ) {
-                button->setFixedSize(10,100);
-                button->setIcon(QIcon(":/images/line.png"));
-		button->setStyleSheet("QPushButton{border-radius: 5px; }");
-                button->setIconSize(QSize(50,50));
-                setFixedSize(10,100);
+            button->setFixedSize(10,100);
+            button->setIcon(QIcon(":/images/line.png"));
+            button->setStyleSheet("QPushButton{border-radius: 5px; }");
+            button->setIconSize(QSize(50,50));
+            setFixedSize(10,100);
         }
-	if ( arrayDirection == RIGHT && !movedRight) {
-		move(reservedRect.width()-10, pos().y());
-		button->setStyleSheet("QPushButton { padding-right: 5px; }");
-		movedRight = true;
-	}
+        if ( arrayDirection == RIGHT && !movedRight) {
+            move(reservedRect.width()-10, pos().y());
+            button->setStyleSheet("QPushButton { padding-right: 5px; }");
+            movedRight = true;
+        }
     }
 
 //新增槽函数
@@ -113,18 +95,18 @@ public slots:
         if (lastPressedTime.msecsTo(QDateTime::currentDateTime()) < 200 ) {
             //判断鼠标左键现在是否依然是按下的
             if (QApplication::mouseButtons() & Qt::LeftButton) { //鼠标依然按下，说明只是焦点离开了按钮，用户依然在长按拖动
-	        return;
+	            return;
             }else if ( !longPressed){ 
-	        qDebug()<< arrayDirection << "button clicked";
+	            qDebug()<< arrayDirection << "button clicked";
                 return moveByClick();
             }
         }
-	longPressed = true;
-	if ((longPressed) && !(QApplication::mouseButtons() & Qt::LeftButton)) { //鼠标左键已然弹
-	    longPressed = false;
-	    dragging = false;
-	    qDebug()<<"floating release by btn";
-	}
+        longPressed = true;
+        if ((longPressed) && !(QApplication::mouseButtons() & Qt::LeftButton)) { //鼠标左键已然弹
+            longPressed = false;
+            dragging = false;
+            qDebug()<<"floating release by btn";
+        }
         return ;
     }
 
@@ -154,13 +136,13 @@ protected:
         process.waitForFinished();
         QString output = process.readAllStandardOutput();
         QStringList windows = output.trimmed().split("\n");
-	int count =  0 ;
+	    int count =  0 ;
         for (const QString& window : windows) {
-	if (count == WindowCount) {
-                    break;
-                }
+            if (count == WindowCount) {
+                break;
+            }
             if (window.simplified().split(" ").at(3).contains(WindowName)) {
-		count++;
+                count++;
                 QString windowId = window.split(" ").first();
                 //将当前窗口移动到当前桌面
                 QProcess sprocess;
@@ -186,7 +168,7 @@ void moveByClick() {
         0  * DG: 1920x1080  VP: 0,0  WA: 0,0 1920x1080  桌面 1   //当前桌面
         1  - DG: 1920x1080  VP: 0,0  WA: 0,0 1920x1080  桌面 2
         2  - DG: 1920x1080  VP: 0,0  WA: 0,0 1920x1034  桌面 3
-    */
+        */
         if (line.simplified().split(" ").at(1).contains("*")) {    //按空格分割，多个空格也视作1个空格
             currentDesktopIndex = line.split(" ").first().toInt();
         }
@@ -196,17 +178,17 @@ void moveByClick() {
     }
     int targetDesktop = 0;
     if (arrayDirection == LEFT) {
-    if (currentDesktopIndex == tmpDesktopList.first()) {
-        targetDesktop = tmpDesktopList.last();
-    } else {
-        targetDesktop = currentDesktopIndex - 1;
-    }
+        if (currentDesktopIndex == tmpDesktopList.first()) {
+            targetDesktop = tmpDesktopList.last();
+        } else {
+            targetDesktop = currentDesktopIndex - 1;
+        }
     } else if (arrayDirection == RIGHT) {
-    if (currentDesktopIndex == tmpDesktopList.size() - 1) {
-        targetDesktop = tmpDesktopList.first();
-    } else {
-        targetDesktop = currentDesktopIndex + 1;
-    }
+        if (currentDesktopIndex == tmpDesktopList.size() - 1) {
+            targetDesktop = tmpDesktopList.first();
+        } else {
+            targetDesktop = currentDesktopIndex + 1;
+        }
     }
     moveToDesktop(targetDesktop);
     moveToCurrentDesktop();
