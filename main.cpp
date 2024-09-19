e93ea62c737677eb8efbfb79ce2aee68959c1c18
gongyong <185457686@qq.com>
2024-09-19 16:46:48 +0800

    add trayicon

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <QWidget>
 #include <QPushButton>
+#include <QSystemTrayIcon>
+#include <QAction>
+#include <QMenu>
 #include <QTimer>
 #include <QMouseEvent>
 #include <QApplication>
@@ -256,6 +259,14 @@ private:
 int main(int argc , char * argv[]){
     QApplication app(argc, argv);
 
+       //新增一个托盘图标功能
+    QSystemTrayIcon *trayIcon = new QSystemTrayIcon(QIcon(":/images/dualArray.jpg"), &app);
+    QMenu *trayMenu = new QMenu();
+    QAction *quitAction = new QAction("Exit FDE Navigator", &app);
+    QObject::connect(quitAction, &QAction::triggered, &app, &QApplication::quit);
+    trayMenu->addAction(quitAction);
+    trayIcon->setContextMenu(trayMenu);
+    trayIcon->show();
     QRect screenRect = QGuiApplication::primaryScreen()->geometry();
 
     DraggableButton *rbtn = new DraggableButton(RIGHT,":/images/right.png",screenRect);
@@ -265,15 +276,12 @@ int main(int argc , char * argv[]){
     lbtn->setWindowOpacity(0.8);
     lbtn->setStyleSheet("background-color: rgba(192,192,192, 0.8);");
 
-//	rbtn->setStyleSheet("QWidget{border-radius: 50px;  }");
-    //设置draggableButton的大小为100 * 100
     rbtn->setFixedSize(100, 100);
     lbtn->setFixedSize(100, 100);
     //rbtn 移动到屏幕的右侧中间
     rbtn->move(screenRect.width() - rbtn->width(), screenRect.height() / 2 - rbtn->height() / 2);
     //lbtn 移动到屏幕的左侧中间
     lbtn->move(0, screenRect.height() / 2 - lbtn->height() / 2);
-    //设置draggableButton的另一个按钮为lbtn
     rbtn->setOtherButton(lbtn);
     lbtn->setOtherButton(rbtn);
     lbtn->show();
